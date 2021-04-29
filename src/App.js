1d2cca1f69a834e4a28e80abf6ea4cf57ff47b68
Saheel Das <saheeldas75@gmail.com>
2021-04-29 20:57:24 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,37 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from 'react';
+import Switch from "react-switch";
+import Header from './Header.js';
 
-function App() {
+const App = () => {
+
+  const [darkMode, setDarkMode] = useState(false);
+  const [style, setStyle] = useState({
+    'backgroundColor': '#dee4e7',
+    'color': '#243447'
+  })
+
+  useEffect(() => {
+    {darkMode ? setStyle({
+      'backgroundColor': '#243447',
+      'color': '#d1d5da'
+    }) : setStyle({
+      'backgroundColor': '#dee4e7',
+      'color': '#243447'
+    })}
+  },[darkMode]);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="main" style={style}>
+      <Header />
+      <h1 className="header">{darkMode ? 'Dark' : 'Light'} Mode</h1>
+      <Switch className="toggle" 
+        onChange={() => setDarkMode(!darkMode)} checked={darkMode} 
+        checkedIcon={darkMode} 
+        uncheckedIcon={darkMode}
+        onColor={'#61DBFB'}
+        handleDiameter={50}
+        width={90}
+        height={35}/>
     </div>
   );
 }
---
+++ src/Header.js
@@ -0,0 +1,14 @@
+const Header = () => {
+    return (
+        <div className="nav">
+            <h2 className="logo">Saheel Das</h2> 
+            <ul>
+                <li><a href='#'>About Me</a></li>
+                <li><a href='https://www.linkedin.com/in/saheeldas21/'>linkedin</a></li>
+                <li><a href='https://github.com/xSaheel'>GitHub</a></li>
+            </ul>
+        </div>
+    )
+}
+
+export default Header;
